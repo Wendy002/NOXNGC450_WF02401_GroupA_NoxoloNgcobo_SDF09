02eebffe068a6e3dc9ba18662959ada9a2b5afca
Noxolo <wendyngcobo98@gmail.com>
2024-02-17 08:27:25 +0200

    updaate footer

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'iphone': {'min': '375px', 'max': '667px'},
+      // => @media (min-width: 640px) { ... }
+
+      'ipad':{'min': '763px', 'max': '1024px'},
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': {'min': '1050px', 'max': '1280px'}
+      // => @media (min-width: 1280px) { ... }
+    },
+    
+  },
+  plugins: [],
+}
+
