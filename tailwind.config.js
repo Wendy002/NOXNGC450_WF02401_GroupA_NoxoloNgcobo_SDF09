a30b990abe1492582d7b5c42a61ab3a3b3a79e7b
Noxolo <wendyngcobo98@gmail.com>
2024-02-19 11:09:54 +0200

    Edit stylecss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./build/**/*.{html,js}"],
   theme: {
     screens: {
       'iphone': {'min': '375px', 'max': '667px'},
